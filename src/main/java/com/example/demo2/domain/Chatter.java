76025c737289baa13cecf20e1ade37e87de87ac4
world-dv <goodforx0147@naver.com>
2024-09-25 23:23:38 +0900

    feat: add dependencies and create doamin

--- src/main/java/com/example/demo2/domain/Chatter.java
+++ src/main/java/com/example/demo2/domain/Chatter.java
@@ -1,17 +1,18 @@
 package com.example.demo2.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-//@Entity
+@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class user {
+public class Chatter {
+
+    @Id
+    private String id;
 
     private String nickname;
-
-    public String getNickName() {
-        return nickname;
-    }
 }
