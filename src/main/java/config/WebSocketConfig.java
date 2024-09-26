af7d03778f19d6275200637d208a6389a76ce35e
world-dv <goodforx0147@naver.com>
2024-09-26 23:09:01 +0900

    feat: set web socekt and config

---
+++ src/main/java/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/chatRoom") // chatRoom 경로로 WebSocket 연결 허용
+                .setAllowedOrigins("*"); //CORS 허용
+    }
+}
