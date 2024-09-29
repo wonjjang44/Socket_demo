488cdff92d8d5b86dda0b59da7a20a8d3139a351
world-dv <goodforx0147@naver.com>
2024-09-30 02:25:10 +0900

    fix: to avoid CORS error(500)

--- src/main/java/com/example/demo2/config/WebSockConfig.java
+++ src/main/java/com/example/demo2/config/WebSockConfig.java
@@ -18,6 +18,6 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*").withSockJS();
     }
 }
