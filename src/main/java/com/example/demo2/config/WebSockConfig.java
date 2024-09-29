6e6604a48b808cc85451ddefa83f811e607bdbf8
world-dv <goodforx0147@naver.com>
2024-09-29 15:23:17 +0900

    fix: change config to use stomp

--- src/main/java/com/example/demo2/config/WebSockConfig.java
+++ src/main/java/com/example/demo2/config/WebSockConfig.java
@@ -2,19 +2,22 @@ package com.example.demo2.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
-@RequiredArgsConstructor
 @Configuration
-@EnableWebSocket
-public class WebSockConfig implements WebSocketConfigurer {
-    private final WebSocketHandler webSocketHandler;
+@EnableWebSocketMessageBroker
+public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry brokerRegistry) {
+        brokerRegistry.enableSimpleBroker("/sub"); //메시지 수신 요청
+        brokerRegistry.setApplicationDestinationPrefixes("/pub"); //메시지 수신 요청
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*").withSockJS();
     }
 }
