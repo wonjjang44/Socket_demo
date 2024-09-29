3639d1c10f54a361d6d692ed6e3b3ed752b283b8
world-dv <goodforx0147@naver.com>
2024-09-30 02:29:41 +0900

    fix: remove unuse import

--- src/main/java/com/example/demo2/service/ChatService.java
+++ src/main/java/com/example/demo2/service/ChatService.java
@@ -1,19 +1,14 @@
 package com.example.demo2.service;
 
 import com.example.demo2.domain.ChatRoom;
-import com.example.demo2.dto.MessageDto;
 import com.example.demo2.repository.ChatRoomRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
