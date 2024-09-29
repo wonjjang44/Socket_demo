08911af7d6ccbf100cc155a1eefb318aba499e28
world-dv <goodforx0147@naver.com>
2024-09-29 23:57:37 +0900

    feat: to solve CORS error

---
+++ src/main/java/com/example/demo2/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo2.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .maxAge(3600);
+    }
+}
