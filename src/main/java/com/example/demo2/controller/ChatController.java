0a8a174e86f79392bfbb633c790a15360f03e086
world-dv <goodforx0147@naver.com>
2024-09-29 23:59:12 +0900

    fix: rename url and rest controller -> controller

--- src/main/java/com/example/demo2/controller/ChatController.java
+++ src/main/java/com/example/demo2/controller/ChatController.java
@@ -1,26 +1,18 @@
 package com.example.demo2.controller;
 
-import com.example.demo2.domain.ChatRoom;
-import com.example.demo2.dto.AddChatRoomDto;
-import com.example.demo2.dto.ChatRoomResponse;
 import com.example.demo2.dto.MessageDto;
-import com.example.demo2.service.ChatService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.stereotype.Controller;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class ChatController {
 
     private final SimpMessageSendingOperations simpMessageSendingOperations;
 
-    @MessageMapping("/hello")
+    @MessageMapping("/chat/message")
     public void message(MessageDto messageDto) {
         if (MessageDto.MessageType.ENTER.equals(messageDto.getType())) {
             messageDto.setMessage(messageDto.getId() + "님이 입장하셨습니다.");
