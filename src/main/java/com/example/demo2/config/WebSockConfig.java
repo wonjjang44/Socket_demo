8bb6b3a1290acd2ae256ae4aeabec51df9d3d4ca
world-dv <goodforx0147@naver.com>
2024-10-02 23:52:27 +0900

    fix: allow '*' -> localhost to protect server

--- src/main/java/com/example/demo2/config/WebSockConfig.java
+++ src/main/java/com/example/demo2/config/WebSockConfig.java
@@ -18,6 +18,6 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("http://localhost:8080").withSockJS();
     }
 }
